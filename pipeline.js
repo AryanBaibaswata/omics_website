8ab7685e7c8373b99c8dc5baccd81eec20885838
CrispyLemon <aryanb349@gmail.com>
2024-07-03 10:21:53 +0530

    saving progress

--- pipeline.js
+++ pipeline.js
@@ -144,55 +144,55 @@ app.post('/upload', upload, (req, res) => {
 
         for sample_name in "\${samples[@]}"; do
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step I: Quality Control and Preprocessing" >> \${progress_file}
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step 1.1: FastQC Quality Control Report for \${sample_name}" >> \${progress_file}
+            echo "Step I: Quality Control and Preprocessing" >> \${progress_file}
+            echo "Step 1.1: FastQC Quality Control Report for \${sample_name}" >> \${progress_file}
             fastqc -o "\${basedir}/fastqc_output/" "\${basedir}/files/\${sample_name}_1.fastq.gz" "\${basedir}/files/\${sample_name}_2.fastq.gz"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-1.2: Trimmed Quality Control for \${sample_name}" >> \${progress_file}
+            echo "Step-1.2: Trimmed Quality Control for \${sample_name}" >> \${progress_file}
             fastp -i "\${basedir}/files/\${sample_name}_1.fastq.gz" -o "\${basedir}/\${sample_name}_P1.fastq" \
                   -I "\${basedir}/files/\${sample_name}_2.fastq.gz" -O "\${basedir}/\${sample_name}_P2.fastq" \
                   --thread 4 -h "\${basedir}/fastp-\${sample_name}.html" 2> "\${basedir}/fastp-\${sample_name}.log"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step II: Read Alignment for \${sample_name}" >> \${progress_file}
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-2.1: Read Alignment for \${sample_name}" >> \${progress_file}
+            echo "Step II: Read Alignment for \${sample_name}" >> \${progress_file}
+            echo "Step-2.1: Read Alignment for \${sample_name}" >> \${progress_file}
             bowtie2 -p 64 -x "\${GENOMEIDX1}" -1 "\${basedir}/\${sample_name}_P1.fastq" -2 "\${basedir}/\${sample_name}_P2.fastq" -S "\${basedir}/\${sample_name}.sam"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-III: Coverage Analysis for \${sample_name}" >> \${progress_file}
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step 3.1 Conversion Of Sam To BAM File for \${sample_name}" >> \${progress_file}
+            echo "Step-III: Coverage Analysis for \${sample_name}" >> \${progress_file}
+            echo "Step 3.1 Conversion Of Sam To BAM File for \${sample_name}" >> \${progress_file}
             samtools view -b "\${basedir}/\${sample_name}.sam" -o "\${basedir}/\${sample_name}.bam"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.2: Alignment Metrics for \${sample_name}" >> \${progress_file}
+            echo "Step-3.2: Alignment Metrics for \${sample_name}" >> \${progress_file}
               samtools flagstat "\${basedir}/\${sample_name}.bam" > "\${basedir}/\${sample_name}.flagstat.txt"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.3: Conversion of BAM To Sorted BAM for \${sample_name}" >> \${progress_file}
+            echo "Step-3.3: Conversion of BAM To Sorted BAM for \${sample_name}" >> \${progress_file}
             samtools sort "\${basedir}/\${sample_name}.bam" -o "\${basedir}/\${sample_name}.sorted.bam"
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.4: Removing duplicate reads from Sorted Bam Files for \${sample_name}" >> \${progress_file}
+            echo "Step-3.4: Removing duplicate reads from Sorted Bam Files for \${sample_name}" >> \${progress_file}
             samtools rmdup -S "\${basedir}/\${sample_name}.sorted.bam" "\${basedir}/\${sample_name}.duprem.bam"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.5: Deriving Low Coverage Bed File for \${sample_name}" >> \${progress_file}
+            echo "Step-3.5: Deriving Low Coverage Bed File for \${sample_name}" >> \${progress_file}
             samtools depth "\${basedir}/\${sample_name}.duprem.bam" | awk '$3 < 5 {print $1"\t"$2"\t"$3}' > "\${basedir}/coverage_\${sample_name}.txt"
             sleep 2
             input_bam="\${basedir}/coverage_\${sample_name}.txt"
             output_bed="\${basedir}/\${sample_name}.bed"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.6: Extracting start end coordinates of missing read segments for \${sample_name}" >> \${progress_file}
+            echo "Step-3.6: Extracting start end coordinates of missing read segments for \${sample_name}" >> \${progress_file}
             python3 "pipelines/convert_bam_to_bed.py" "\${sample_name}" "\${input_bam}" "\${output_bed}"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-3.7: Performing N-masking for \${sample_name}" >> \${progress_file}
+            echo "Step-3.7: Performing N-masking for \${sample_name}" >> \${progress_file}
             bedtools maskfasta -fi "\${GENOMEIDX}" -bed "\${output_bed}" -mc N -fo "\${basedir}/\${sample_name}_masked.fasta"
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step IV: Generation of VCF, VCF Index and Viral Genome for \${sample_name}" >> \${progress_file}
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-4: Generation of VCF for \${sample_name}" >> \${progress_file}
+            echo "Step IV: Generation of VCF, VCF Index and Viral Genome for \${sample_name}" >> \${progress_file}
+            echo "Step-4: Generation of VCF for \${sample_name}" >> \${progress_file}
             bcftools mpileup -f "\${GENOMEIDX}" "\${basedir}/\${sample_name}.duprem.bam" | bcftools call -cv --ploidy 1 -Oz -o "\${basedir}/\${sample_name}.vcf.gz"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-4.1: Generation of VCF Index for \${sample_name}" >> \${progress_file}
+            echo "Step-4.1: Generation of VCF Index for \${sample_name}" >> \${progress_file}
             bcftools index "\${basedir}/\${sample_name}.vcf.gz"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - Step-4.2: Generation of viral genome fasta for \${sample_name}" >> \${progress_file}
+            echo "Step-4.2: Generation of viral genome fasta for \${sample_name}" >> \${progress_file}
             cat "\${basedir}/\${sample_name}_masked.fasta" | bcftools consensus "\${basedir}/\${sample_name}.vcf.gz" > "\${basedir}/\${sample_name}_genome.fa"
             sleep 2
-            echo "$(date '+%Y-%m-%d %H:%M:%S') - complete!" >> \${progress_file}
+            echo "complete!" >> \${progress_file}
         done
-        echo \$(date '+%Y-%m-%d %H:%M:%S') - Step 1.3: MultiQC Quality Control" >> \${progress_file}
+        echo \Step 1.3: MultiQC Quality Control" >> \${progress_file}
             multiqc "\${basedir}/fastqc_output/" "\${basedir}/" -o "\${basedir}/multiqc_output/"
         `;
 
